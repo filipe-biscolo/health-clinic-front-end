fded1f0828add71d4404e26328e0d6966dc4325a
filipe-biscolo <filipe.biscolo@actionlabs.com.br>
2021-04-07 21:11:00 -0300

    General adjusts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:3333',
+  API_URL: 'https://health-clinic-tcc-back-end.herokuapp.com',
 
   g_client_id: '1047440820874-acsaelv1lfoif3d3pf205rjc6q96cq1f.apps.googleusercontent.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:3333',
+  API_URL: 'https://health-clinic-tcc-back-end.herokuapp.com',
 
   g_client_id: '1047440820874-acsaelv1lfoif3d3pf205rjc6q96cq1f.apps.googleusercontent.com'
 };
